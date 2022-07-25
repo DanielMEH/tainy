e2b075d2abf95354a2c323930434cdc4e1a51d9d
Juan Esteban Correa <jecorreajara@gmail.com>
2022-07-25 06:42:25 -0500

    version2

--- src/controllers/viewsControllers.ts
+++ src/controllers/viewsControllers.ts
@@ -3,11 +3,24 @@ import {Request, Response} from 'express';
 
  class Indexviews 
 {
+   
 
  public userVista(req: Request, res: Response): void {
 
     res.send("<h1>Hola mundo</h1>");
+    
     }
+
+ public userPerfil(req: Request, res: Response): void{
+
+    res.send("<h1>User prfil</h1>")
+
+    }
+
+ public userPublicacion(req: Request, res: Response): void {
+
+    res.send("<h1><h1>")
+ }
     
 }
 
