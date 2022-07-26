048f4d7528404c65de6ef7b4d8402bf8567b0d3d
danielsansp <daniel1008ospina@gmail.com>
2022-07-25 20:40:27 -0500

    avanze2

---
+++ src/public/js/script.js
@@ -0,0 +1,25 @@
+let microphone = document.getElementById("microphone");
+let classInpu = document.getElementById("classInpu");
+microphone.addEventListener("click", listenMicrofono);
+let listent;
+
+function listenMicrofono() {
+  if (!"webkitSpeechRecognition" in window) {
+    alert("¡API no soportada!");
+  } else {
+    listent = new webkitSpeechRecognition();
+    listent.interim = true;
+    listent.lang = "es-ES";
+    listent.continuous = true;
+    listent.addEventListener("result", startListent);
+    listent.start();
+  }
+}
+
+function startListent(event) {
+  for (let i = event.resultIndex; i < event.results.length; i++) {
+    if (event.results[i].isFinal) {
+      classInpu.innerHTML = event.results[i][0].transcript;
+    }
+  }
+}
