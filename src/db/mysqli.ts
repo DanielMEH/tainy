c1819f23025531b2870011bef99bbbc65cc04cc8
Juan Correa <jecorreajara@gmail.com>
2022-08-26 11:49:40 -0500

    data sice

--- src/db/mysqli.ts
+++ src/db/mysqli.ts
@@ -9,7 +9,7 @@ export async function connect() {
             password: "",
             database: "conciertos",
             charset: "utf8",
-            port: 3306
+            port: 3310
             
         })
         return conenct;
---
+++ src/public/js/paralaxx.js
@@ -0,0 +1,98 @@
+const imagenParalaxx = document.querySelector(".conrParalaxx");
+const TitleParalaxx = document.getElementById("title");
+const containerImagen = (entradas, observer)=>{
+
+    entradas.forEach(entrada => {
+        
+        if (entrada.isIntersecting) {
+            
+        window.onscroll = function(){
+        let posision = window.pageYOffset || document.documentElement.scrollTo;
+       
+        const img1 = document.getElementById("img1");
+        const img2 = document.getElementById("img2");
+        const img3 = document.getElementById("img3");
+        const img4 = document.getElementById("img4");
+        const img5 = document.getElementById("img5");
+        const img6 = document.getElementById("img6");
+        img1.style.opacity ="1"
+        img2.style.opacity ="1"
+        img3.style.opacity ="1"
+        img4.style.opacity ="1"
+        img5.style.opacity ="1"
+        img6.style.opacity ="1"
+        img1.style.right = posision * 0.1 + "px"
+        img1.style.transform= `translateY(-${posision * 0.1}px)`;
+        img2.style.bottom = posision * 0.1 + "px";
+        img3.style.left = posision * 0.1 + "px"
+        img3.style.transform= `translateY(-${posision * 0.1}px)`;
+        img5.style.bottom= posision * 0.1+"px";
+        img4.style.right = posision * 0.1 + "px";
+        img4.style.transform= `translateY(${posision * 0.1}px)`;
+        img6.style.transform= `translateY(${posision * 0.1}px)`;
+        img6.style.left = posision * 0.1 + "px"
+        
+    }
+        }else{
+        }
+        
+    });
+}
+
+const observerParalaxx = new IntersectionObserver(containerImagen, {
+
+    root: null,
+    rootMargin: "100px 0px 100px 0px",
+    threshold: 1.0
+})
+observerParalaxx.observe(imagenParalaxx)
+function observerViews(){
+
+}
+const textParalaxxContent = (entradas, observador)=>{
+    entradas.forEach(entrada => {
+        
+        if (entrada.isIntersecting) {
+            
+
+            entrada.target.classList.add("active")
+             entrada.target.classList.remove("delete")
+             
+            }else{
+                
+                entrada.target.classList.remove("active")
+                 entrada.target.classList.add("delete")
+        }
+    })
+
+}
+
+const observerSecond = new IntersectionObserver(textParalaxxContent,{
+    root: null,
+    rootMargin: "0px 0px 0px 0px",
+    threshold: 1.0
+
+})
+observerSecond.observe(TitleParalaxx)
+
+const thirdImage = document.getElementById("thirdImage")
+const observerThirdt = (entradas, observador)=>{
+    entradas.forEach((entrada)=>{
+
+        const imagenk = document.getElementById("imagenK")
+        if (entrada.isIntersecting) {
+            
+        }
+    })
+    
+
+}
+
+
+const newObserverSecond = new IntersectionObserver(observerThirdt, {
+
+    root: null,
+    rootMargin: "0px 0px 0px 0px",
+    threshold: 1.0
+})
+newObserverSecond.observe(thirdImage)
